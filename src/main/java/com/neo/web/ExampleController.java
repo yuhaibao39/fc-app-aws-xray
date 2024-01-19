f085ab3b266d5608a37cdba60601ac063c31aebb
zhishi324 <45934730+zhishi324@users.noreply.github.com>
2024-01-19 19:02:17 +0800

    Update ExampleController.java

--- src/main/java/com/neo/web/ExampleController.java
+++ src/main/java/com/neo/web/ExampleController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ExampleController {
+public class AppController {
 
     private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
 
