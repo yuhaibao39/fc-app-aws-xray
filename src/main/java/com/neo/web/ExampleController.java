12edb9f338d93ee382a015ace704ecff176cfea4
zhishi324 <45934730+zhishi324@users.noreply.github.com>
2024-01-19 19:00:50 +0800

    Update ExampleController.java

--- src/main/java/com/neo/web/ExampleController.java
+++ src/main/java/com/neo/web/ExampleController.java
@@ -1,20 +1,23 @@
 package com.neo.web;
 
-import com.neo.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 @Controller
 public class ExampleController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
     @RequestMapping("/string")
     public String string(ModelMap map) {
+
         map.addAttribute("userName", "ityouknow");
+
+        logger.info("ExampleController");
+
         return "string";
     }
 
@@ -22,12 +25,6 @@ public class ExampleController {
     public String ifunless(ModelMap map) {
         map.addAttribute("flag", "yes");
         return "if";
-    }
-
-    @RequestMapping("/list")
-    public String list(ModelMap map) {
-        map.addAttribute("users", getUserList());
-        return "list";
     }
 
     @RequestMapping("/url")
@@ -51,16 +48,4 @@ public class ExampleController {
         map.addAttribute("sex", "woman");
         return "switch";
     }
-
-    private List<User> getUserList(){
-        List<User> list=new ArrayList<User>();
-        User user1=new User("大牛",12,"123456");
-        User user2=new User("小牛",6,"123563");
-        User user3=new User("Bosicloud",66,"666666");
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        return  list;
-    }
-
 }
